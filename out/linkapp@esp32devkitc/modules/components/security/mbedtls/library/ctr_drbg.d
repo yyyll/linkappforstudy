8686173c7c798cfb52839efd150422d3ef31e3bb
XuBovey <xu.bovey@foxmail.com>
2020-11-10 07:41:25 +0800

    删除无用文件，增加.gitignore文件

--- out/linkapp@esp32devkitc/modules/components/security/mbedtls/library/ctr_drbg.d
+++
@@ -1,9 +0,0 @@
-F:/EDU/iot_esp32/git/prj/linkapp/out/linkapp@esp32devkitc/modules//components/security/mbedtls/library/ctr_drbg.o: \
- F:/EDU/iot_esp32/git/AliOS-Things/components/security/mbedtls/library/ctr_drbg.c \
- F:\EDU\iot_esp32\git\prj\linkapp/aos_config.h \
- F:/EDU/iot_esp32/git/AliOS-Things/components/security/mbedtls/aos/include/mbedtls_config.h \
- F:/EDU/iot_esp32/git/AliOS-Things/components/security/mbedtls/include/mbedtls/check_config.h \
- f:\edu\iot_esp32\git\alios-things\build\compiler\gcc-xtensa-esp32\win32\lib\gcc\xtensa-esp32-elf\5.2.0\include-fixed\limits.h \
- f:\edu\iot_esp32\git\alios-things\build\compiler\gcc-xtensa-esp32\win32\lib\gcc\xtensa-esp32-elf\5.2.0\include-fixed\syslimits.h \
- f:\edu\iot_esp32\git\alios-things\build\compiler\gcc-xtensa-esp32\win32\xtensa-esp32-elf\include\limits.h \
- f:\edu\iot_esp32\git\alios-things\build\compiler\gcc-xtensa-esp32\win32\xtensa-esp32-elf\include\newlib.h
--- out/linkapp@esp32devkitc/modules/platform/arch/xtensa/lx6/backtrace.d
+++
@@ -1,3 +0,0 @@
-F:/EDU/iot_esp32/git/prj/linkapp/out/linkapp@esp32devkitc/modules//platform/arch/xtensa/lx6/backtrace.o: \
- F:/EDU/iot_esp32/git/AliOS-Things/platform/arch/xtensa/lx6/backtrace.c \
- F:\EDU\iot_esp32\git\prj\linkapp/aos_config.h
